449a96500487a8142530d3ca1b9f7a68e6f0da5f
reisritter <reisritter@gmail.com>
2023-10-02 00:08:03 -0300

    sapo

--- domain/src/main/java/br/com/boucher/service/FormaPagamentoService.java
+++ domain/src/main/java/br/com/boucher/service/FormaPagamentoService.java
@@ -3,9 +3,6 @@ package br.com.boucher.service;
 import br.com.boucher.entidades.FormaPagamento;
 import br.com.boucher.port.FormaPagamentoDatabasePort;
 import br.com.boucher.port.FormaPagamentoServicePort;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +19,7 @@ public class FormaPagamentoService implements FormaPagamentoServicePort {
 
     @Override
     public FormaPagamento get(UUID id) {
+
         return pagamentoDatabasePort.get(id);
     }
 
